072a999855073049660adf89694ca5ec0d9bc9e8
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-07-25 20:15:42 -0500

    init swipeable

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,44 @@
 import React, { useContext } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { SafeAreaView, View, ScrollView, StyleSheet } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { Text, Button } from 'react-native-elements';
 import { LocationContext } from '../contexts';
 import { useDate } from '../hooks';
 import PullToRefresh from '../components/PullToRefresh';
+
+const LeftActions = () => {
+  return (
+    <View
+      style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center' }}
+    >
+      <Text
+        style={{
+          color: 'white',
+          paddingHorizontal: 10,
+          fontWeight: '600',
+        }}
+      >
+        Left Action
+      </Text>
+    </View>
+  );
+};
+
+const RightActions = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center' }}>
+      <Text
+        style={{
+          color: 'white',
+          paddingHorizontal: 10,
+          fontWeight: '600',
+        }}
+      >
+        Right Action
+      </Text>
+    </View>
+  );
+};
 
 const Home: React.FC = () => {
   const { city } = useContext(LocationContext);
@@ -20,10 +55,15 @@ const Home: React.FC = () => {
         <Text h1 style={styles.cityStyles}>
           {city || 'Chicago'}
         </Text>
-        <Text style={styles.dateStyles}>{formattedDate}</Text>
-        <Button onPress={subtractOneDay} title="Subtract a Day" />
-        <Text></Text>
-        <Button onPress={addOneDay} title="Add a Day" />
+        <Swipeable
+          renderLeftActions={LeftActions}
+          renderRightActions={RightActions}
+        >
+          <Text style={styles.dateStyles}>{formattedDate}</Text>
+          <Button onPress={subtractOneDay} title="Subtract a Day" />
+          <Text></Text>
+          <Button onPress={addOneDay} title="Add a Day" />
+        </Swipeable>
       </ScrollView>
     </SafeAreaView>
   );
