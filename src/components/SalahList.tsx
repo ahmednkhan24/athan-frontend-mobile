0547ec0eda6fab31ae8b7b22a1652fd21aef9aa2
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-07-31 20:39:45 -0500

    fix virtualized list

--- src/components/SalahList.tsx
+++ src/components/SalahList.tsx
@@ -1,6 +1,45 @@
-import React from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { Text, Button } from 'react-native-elements';
 import SalahItem from './SalahItem';
+import PullToRefresh from './PullToRefresh';
+import { LocationContext } from '../contexts';
+import { useDate } from '../hooks';
+
+const LeftActions = () => {
+  return (
+    <View
+      style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center' }}
+    >
+      <Text
+        style={{
+          color: 'white',
+          paddingHorizontal: 10,
+          fontWeight: '600',
+        }}
+      >
+        Left Action
+      </Text>
+    </View>
+  );
+};
+
+const RightActions = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center' }}>
+      <Text
+        style={{
+          color: 'white',
+          paddingHorizontal: 10,
+          fontWeight: '600',
+        }}
+      >
+        Right Action
+      </Text>
+    </View>
+  );
+};
 
 const salahs = [
   { title: 'Fajr', time: '4:41 am' },
@@ -11,22 +50,51 @@ const salahs = [
 ];
 
 const SalahList: React.FC = () => {
+  const { city } = useContext(LocationContext);
+  const { formattedDate, subtractOneDay, addOneDay } = useDate();
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={salahs}
-        keyExtractor={(salah) => salah.title}
-        renderItem={({ item }) => (
-          <SalahItem title={item.title} time={item.time} />
-        )}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Swipeable
+        renderLeftActions={LeftActions}
+        renderRightActions={RightActions}
+      >
+        <FlatList
+          refreshControl={<PullToRefresh />}
+          ListHeaderComponent={
+            <>
+              <Text h1 style={styles.cityStyles}>
+                {city || 'Chicago'}
+              </Text>
+              <Text style={styles.dateStyles}>{formattedDate}</Text>
+              <Button onPress={subtractOneDay} title="Subtract a Day" />
+              <Text></Text>
+              <Button onPress={addOneDay} title="Add a Day" />
+            </>
+          }
+          data={salahs}
+          keyExtractor={(salah) => salah.title}
+          renderItem={({ item }) => (
+            <SalahItem title={item.title} time={item.time} />
+          )}
+        />
+      </Swipeable>
+    </SafeAreaView>
   );
 };
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  cityStyles: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  dateStyles: {
+    flex: 1,
+    justifyContent: 'flex-start',
   },
 });
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,94 +1,19 @@
-import React, { useContext } from 'react';
-import { SafeAreaView, View, ScrollView, StyleSheet } from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
-import { Text, Button } from 'react-native-elements';
-import { LocationContext } from '../contexts';
-import { useDate } from '../hooks';
-import PullToRefresh from '../components/PullToRefresh';
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import SalahList from '../components/SalahList';
 
-const LeftActions = () => {
-  return (
-    <View
-      style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center' }}
-    >
-      <Text
-        style={{
-          color: 'white',
-          paddingHorizontal: 10,
-          fontWeight: '600',
-        }}
-      >
-        Left Action
-      </Text>
-    </View>
-  );
-};
-
-const RightActions = () => {
-  return (
-    <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center' }}>
-      <Text
-        style={{
-          color: 'white',
-          paddingHorizontal: 10,
-          fontWeight: '600',
-        }}
-      >
-        Right Action
-      </Text>
-    </View>
-  );
-};
-
-const Home: React.FC = () => {
-  const { city } = useContext(LocationContext);
-  const { formattedDate, addOneDay, subtractOneDay } = useDate();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-        refreshControl={<PullToRefresh />}
-      >
-        <Text h1 style={styles.cityStyles}>
-          {city || 'Chicago'}
-        </Text>
-        <Swipeable
-          renderLeftActions={LeftActions}
-          renderRightActions={RightActions}
-        >
-          <Text style={styles.dateStyles}>{formattedDate}</Text>
-          <Button onPress={subtractOneDay} title="Subtract a Day" />
-          <Text></Text>
-          <Button onPress={addOneDay} title="Add a Day" />
-          <SalahList />
-        </Swipeable>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const Home: React.FC = () => (
+  <SafeAreaView style={styles.container}>
+    <SalahList />
+  </SafeAreaView>
+);
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  scrollView: {
-    flexGrow: 1,
     marginLeft: 15,
     marginRight: 15,
-  },
-  cityStyles: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  dateStyles: {
-    flex: 1,
-    justifyContent: 'flex-start',
   },
 });
 
