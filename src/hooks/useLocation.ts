4974b4c3d885d91f4192eea5197fb3f70e441de8
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-08-22 14:49:20 -0500

    useLocation hook

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { calculateCoordinates } from '../store/actions';
 import type { UseLocation, RootState } from '../types';
@@ -6,6 +7,11 @@ export const useLocation: UseLocation = () => {
   const dispatch = useDispatch();
   const location = useSelector(({ location }: RootState) => location);
   const recalculateLocation = () => dispatch(calculateCoordinates());
+
+  // calculate location on initial render
+  useEffect(() => {
+    recalculateLocation();
+  }, []);
 
   return { location, recalculateLocation };
 };
